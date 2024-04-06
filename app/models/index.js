d13bbb75d6ac76c3c9a2018e8f39f951aa57f30f
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-04-06 18:07:45 +0800

    Delete index.js file in models folder

--- app/models/index.js
+++
@@ -1,8 +0,0 @@
-const dbConfig = require("../config/database.js");
-const mongoose = require("mongoose");
-
-module.exports = {
-  mongoose,
-  url: dbConfig.url,
-  admin: require("./admin.models.js")(mongoose),
-};
