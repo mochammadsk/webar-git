577f07593f2218338b01bbc6e9cf31a4406e4e7a
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-04-07 10:34:46 +0800

    Change hashing method bcrypt to argon2

--- app/controllers/user.controllers.js
+++ app/controllers/user.controllers.js
@@ -1,18 +1,18 @@
 const User = require("../models/user.models");
 const response = require("../config/response");
-const bcrypt = require("bcrypt");
+const argon2 = require("argon2");
 
 exports.register = (data) =>
   new Promise((resolve, reject) => {
     User.findOne({ userName: data.userName })
       .then((user) => {
         if (user) {
-          resolve(response.commonErrorMsg("username already exists!"));
+          reject(response.commonErrorMsg("Username already exists!"));
         } else {
-          bcrypt.hash(data.password, 10, (err, hash) => {
-            if (err) {
-              reject(response.commonErrorMsg);
-            } else {
+          // Hash password
+          argon2
+            .hash(data.password)
+            .then((hash) => {
               data.password = hash;
               User.create(data)
                 .then(() =>
@@ -21,11 +21,13 @@ exports.register = (data) =>
                 .catch(() =>
                   reject(response.commonErrorMsg("Registration failed!"))
                 );
-            }
-          });
+            })
+            .catch(() =>
+              reject(response.commonErrorMsg("Password hashing failed!"))
+            );
         }
       })
-      .catch(() => reject(response.commonErrorMsg));
+      .catch(() => reject(response.commonErrorMsg("Failed to find user!")));
   });
 
 exports.login = (data) =>
@@ -34,11 +36,19 @@ exports.login = (data) =>
       userName: data.userName,
     }).then((user) => {
       if (user) {
-        if (bcrypt.compareSync(data.password, user.password)) {
-          resolve(response.commonResult(user));
-        } else {
-          reject(response.commonErrorMsg("Wrong password!"));
-        }
+        // Verifikasi password
+        argon2
+          .verify(user.password, data.password)
+          .then((match) => {
+            if (match) {
+              resolve(response.commonResult(user));
+            } else {
+              reject(response.commonErrorMsg("Wrong password!"));
+            }
+          })
+          .catch(() => {
+            reject(response.commonErrorMsg("Failed to verify password!"));
+          });
       } else {
         reject(response.commonErrorMsg("Username not found!"));
       }
