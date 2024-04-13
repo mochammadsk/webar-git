5b900aaa4d09da34a49ab752f403d18f6ec903fd
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-04-13 15:36:13 +0800

    Add middelware for ejs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
+const flash = require("connect-flash");
 const path = require("path");
 
 const app = express();
@@ -9,13 +10,23 @@ const corsOption = {
   origin: "*",
 };
 
+const session = require("express-session");
+app.use(
+  session({
+    secret: "rahasia",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 // Middelware
+app.use(flash());
 app.use(express.json());
 app.use(cors(corsOption));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "app/views"));
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   bodyParser.json({
