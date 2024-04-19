f7003034ebe5cb894047bfda8f80fa1f6f93667d
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-04-19 10:41:46 +0800

    Fix configure database

--- app/config/database.js
+++ app/config/database.js
@@ -1,11 +1,5 @@
-const mongoose = require("mongoose");
-
 const dbConfig = {
   url: "mongodb://127.0.0.1:27017/webar_git",
-  mongooseConfig: {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-  },
 };
 
 module.exports = dbConfig;
