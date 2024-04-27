4aa43285f1cec26fd280378198fc1a6b65bf9cdf
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-04-28 00:07:07 +0800

    Fix some bug routes

--- app/controllers/subscription.controller.js
+++ app/controllers/subscription.controller.js
@@ -1,4 +1,4 @@
-const Product = require("../models/Product");
+const Product = require("../models/product.models");
 const Subscription = require("../models/subscription.models");
 
 exports.subscribe = async (req, res) => {
